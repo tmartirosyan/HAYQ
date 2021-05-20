11cc9c771d9b5431e5eebd5709f5d649529d74cd
tmartirosyan <41955326+tmartirosyan@users.noreply.github.com>
2021-05-20 20:37:39 +0400

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<title>404 not found</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<style>
+body,h1 {font-family: "Raleway", sans-serif}
+body, html {height: 100%}
+</style>
+<body>
+
+<div class="w3-display-container">
+  <div class="w3-display-middle"style="margin: 175px 0;">
+    <h1 class="w3-jumbo w3-animate-top">404 not found</h1>
+    <hr class="w3-border-grey" style="margin:auto;width:40%">
+  </div>
+</div>
+
+</body>
+</html>
